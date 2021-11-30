1265e92ce9f60f332fb9a59616b1a0cc3caeaed6
Matteo Vidali <mvidali@iu.edu>
2021-11-30 13:02:30 -0500

    First Commit!

---
+++ Monitoring/plot_data.py
@@ -0,0 +1,69 @@
+import sched
+import time
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+# get csv file
+file = input("filename: ")
+
+# read chunk from csv file
+
+# Set chunk size
+CHUNK_SIZE = 100
+
+# set rate in hz to read chunk from file
+read_freq = 10
+
+
+# read and plot that chunk
+
+    
+
+def animate(i):
+    data = pd.read_csv(file)
+    x = data["index"]
+    ecg = data["ecg_value"]
+    gsr = data["gsr_value"]
+
+
+    plt.cla()
+    plt.plot(x, ecg, label="ecg")
+    plt.plot(x, gsr, label="gsr")
+
+    plt.legend(loc="upper left")
+    plt.tight_layout()
+
+
+fig, axs = plt.subplots(5)
+
+def animate_stacked(i):
+    data = pd.read_csv(file)
+    x = data["index"]
+    ecg = data["ecg_value"]
+    gsr = data["gsr_value"]
+    q_stat = data["q_status"]
+    a_stat = data["a_status"]
+
+
+    axs[0].cla()
+    axs[1].cla()
+    axs[2].cla()
+    axs[3].cla()
+    axs[4].cla()
+
+    starting_point = len(x) - 3000
+    axs[0].plot(x[starting_point:], ecg[starting_point:], label="ecg")
+    axs[1].plot(x[starting_point:], gsr[starting_point:], label="gsr")
+    axs[2].plot(x[starting_point:], ecg[starting_point:], label="stacked")
+    axs[2].plot(x[starting_point:], gsr[starting_point:])
+    axs[3].plot(x[starting_point:], a_stat[starting_point:], label = "a_stat")
+    axs[4].plot(x[starting_point:], q_stat[starting_point:], label = "q_stat")
+    
+    fig.legend(loc="upper left")
+
+    
+    
+ani = FuncAnimation(fig, animate_stacked, interval=10)
+
+plt.show()
---
+++ Monitoring/readSerial.py
@@ -0,0 +1,139 @@
+import serial
+import os
+import csv
+import keyboard
+
+
+def is_connected(port):
+    return os.path.exists(f'/dev/{port}')
+
+def connect_to_serial(baudrate=115200):
+
+    PORT = input("Type the port you are tryig to connect to: ")
+
+    while(1):
+        print(f'\n\nReading in from: {PORT}')
+
+        if not is_connected(PORT):
+
+            print("Nothing is connected at that port location.\n\n")
+            PORT = input("Type a new port to connect to " 
+                         "(Press enter to close)...:")
+
+            if not PORT:
+                return None
+
+        try:
+            ser = serial.Serial(f'/dev/{PORT}', \
+                                baudrate=baudrate, \
+                                parity=serial.PARITY_NONE, \
+                                stopbits=serial.STOPBITS_ONE,\
+                                bytesize=serial.EIGHTBITS,\
+                                timeout=1)
+            ser.read_all()
+            return ser
+
+        except:
+            raise Exception("ERROR WITH SETTING UP SERIAL CONNECTION")
+        
+
+def read_serial(serialObj):
+    
+    #Set DTR (Figure out what does)
+    #serialObj.setDTR(1)
+
+    #Read line
+    line = serialObj.readline()
+    
+    if not line:
+        return None
+    #Decode Line
+    
+    decodedLine = line.decode("utf-8")
+    
+    #Return String
+    return decodedLine 
+
+
+def write_serial_to_csv(filename, serial_obj):
+    fieldnames = ["index", "ecg_value", "gsr_value", "q_status", "a_status"]
+    idx = 0 
+    ecg = None
+    gsr = None
+    q_status = False
+    a_status = False
+
+    with open(filename, "w") as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        csv_writer.writeheader()
+    
+    csv_file = open(filename, 'a')
+
+    while True:
+        try:
+            try:
+                ecg, gsr = read_serial(serial_obj).strip().split(',')
+            except AttributeError:
+                print("ecg, gsr, are not defined\n")
+                continue
+
+            if not ecg and gsr:
+                print("ecg and gsr are none")
+                continue
+            
+            q_status = 1 * keyboard.is_pressed('i')
+            a_status = 1 * keyboard.is_pressed('a')
+            print(ecg, gsr, q_status, a_status)
+
+            csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+            info = {
+                "index": idx,
+                "ecg_value": ecg,
+                "gsr_value": gsr,
+                "q_status": q_status,
+                "a_status": a_status
+            }
+            
+            csv_writer.writerow(info)
+            idx += 1
+
+        except KeyboardInterrupt:
+            print("KEYBOARDINTERRUPT")
+            break
+
+    print("ENDING TRANSMISSION LINE")
+    csv_file.close()
+    return 0
+        
+
+def main():
+    
+    # Connect to serial
+
+
+    # Add to csv file fast
+    log_file = input("Enter a filename to store log in:")
+
+    serial = connect_to_serial()
+    
+
+    if not serial:
+        print("issue connecting")
+        return 0 
+    
+    print("CONNECTED SUCCESSFULLY")
+
+    write_serial_to_csv(log_file, serial)
+
+     
+    print("closing")
+
+    serial.close()
+    return 0
+
+
+#-------------------- __name__ -- __main__ --------------------
+
+if __name__ == "__main__":
+    main()
